c17b13fc913ceb78ecc8febbd8d7e8c7b0dbf9d1
AntSpanish <114097276+AntSpanish@users.noreply.github.com>
2022-12-05 22:21:20 -0500

    Python Queue Simulation

---
+++ PythonQueue.py
@@ -0,0 +1,12 @@
+#Name = Ant Spanish
+#PythonQueue.py
+
+names = []
+for x in range(10):
+    acceptedName=str(input('Enter name: '))
+    names.append(acceptedName)
+print(names)
+for x in range(10):
+    len(names)
+    print(names.pop(0))
+print(names)
